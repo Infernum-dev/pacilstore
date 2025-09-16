4f52142f5e875f4890cd4d097efc5dee19660c0c
Jovian12 <infernum212@gmail.com>
2025-09-16 19:56:29 +0700

    Solve masalah CSRF

--- pacil_store/settings.py
+++ pacil_store/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jovian-felix-pacilstore.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://jovian-felix-pacilstore.pbp.cs.ui.ac.id/"
+    "https://jovian-felix-pacilstore.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
